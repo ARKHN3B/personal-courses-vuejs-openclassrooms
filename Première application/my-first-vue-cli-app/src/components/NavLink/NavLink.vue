95e3e8230733e348f5cd448b28ea80cdc2fe1981
Lmsc <29871193+ARKHN3B@users.noreply.github.com>
2021-03-11 15:10:57 +0100

    firsst vue app by the cli

---
+++ Première application/my-first-vue-cli-app/src/components/NavLink/NavLink.vue
@@ -0,0 +1,14 @@
+<template>
+  <a :href="url">{{ text }}</a>
+</template>
+
+<script>
+export default {
+  name: "NavLink",
+  props: ["url", "text"], // 07) Voici comment définir des props au niveau d'un composant Vue
+};
+</script>
+
+<style scoped>
+
+</style>
